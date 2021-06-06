bfb54cd1d1ac1099499c869830f5e73088a24352
margoar <marcelapaz.ga@gmail.com>
2021-06-06 18:43:37 -0400

    creacion entidad alumno

---
+++ MiPrimerProyectoMVC/Model/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Alumno
+    {
+        private int id { get; set; }
+        private string Nombre { get; set; }
+        public static List<Alumno> Listar()
+        {
+            return null;
+        }
+    }
+}
